d29181244acdd39cc732532b943d37a97c0e0ea4
John Y <johnyu2@gmail.com>
2013-04-07 14:34:26 -0400

    schema update for transactions and messages

--- railsapp/db/schema.rb
+++ railsapp/db/schema.rb
@@ -11,7 +11,37 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130407175354) do
+ActiveRecord::Schema.define(:version => 20130407183105) do
+
+  create_table "messages", :force => true do |t|
+    t.string   "type"
+    t.string   "reply"
+    t.text     "subject"
+    t.text     "body"
+    t.string   "captcha_id"
+    t.string   "captcha_sol"
+    t.boolean  "sent"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  create_table "transactions", :force => true do |t|
+    t.string   "transaction_id"
+    t.string   "sender_user_name"
+    t.string   "sender_user_address"
+    t.string   "receiver_user_name"
+    t.string   "receiver_user_address"
+    t.decimal  "amount_btc"
+    t.decimal  "amount_usd"
+    t.string   "type"
+    t.string   "url"
+    t.string   "subreddit"
+    t.datetime "verified_at"
+    t.string   "status_message"
+    t.string   "status"
+    t.datetime "created_at",            :null => false
+    t.datetime "updated_at",            :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "user_name"
