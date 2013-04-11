21eb9557dae69d5752c4a48d9cce21c03352cbe9
John Y <johnyu2@gmail.com>
2013-04-11 08:45:58 -0400

    modify db column names for user

--- railsapp/db/migrate/20130407175354_create_users.rb
+++ railsapp/db/migrate/20130407175354_create_users.rb
@@ -1,8 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
-      t.string :user_name
-      t.string :address
+      t.string :reddit_user_name
+      t.string :uuid
+      t.string :bitcoin_address
       t.decimal :balance
       t.decimal :total_gift_amount
 
